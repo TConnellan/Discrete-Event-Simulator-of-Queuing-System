1c2f0ab1fab22c7ab4600930bc13d0962b3e439d
Thomas Connellan <thomasjconnellan@gmail.com>
2021-10-13 19:44:28 +1000

    capability to track jobs in mode 1 removed

--- src/state.jl
+++ src/state.jl
@@ -13,6 +13,10 @@ mutable struct TrackAllJobs <: State
     currentPosition::Dict{Int64, Tuple{Float64, Int64}}
     sojournPush::Vector{Float64}
 
+    # may not need currentPosition, just have Dict or arr with entry times
+    # and a collection of jobs in transit
+    # maybe storing currentPosition will just be faster who knows
+
     buffers::Vector{Queue{Int64}}
     jobCount::Int64
 end
@@ -22,13 +26,17 @@ mutable struct TrackTotals <: State
     atNodes::Vector{Int64}
     transit::Int64
 
-    buffers::Vector{Queue{Int64}}
-    jobCount::Int64
+    #buffers::Vector{Queue{Int64}}
+    #jobCount::Int64
 end
 # -----------
 
-function new_job(state::State)::Int64
+function new_job(state::TrackAllJobs)::Int64
     return state.jobCount += 1
+end
+
+function new_job(state::TrackTotals)::Int64
+    return 1
 end
 
 # -----------
@@ -38,18 +46,19 @@ function job_join_sys(job::Int64, node::Int64, time::Float64, state::TrackAllJob
     state.currentPosition[job] = (time, -1)
 end   
 
-function job_join_sys(job::Int64, node::Int64, time::Float64, state::TrackTotals)
-    state.transit += 1
+function job_join_sys(job::Int64, node::Int64, time::Float64, state::TrackTotals)::Int64
+    return state.transit += 1
 end
 
 function job_leave_sys end
 
-function job_leave_sys(job::Int64, node::Int64, time::Float64, state::TrackAllJobs)
+function job_leave_sys(job::Int64, node::Int64, time::Float64, state::TrackAllJobs)::Nothing
     push!(state.sojournPush, time - arr_time(job, state))
     delete!(state.currentPosition, job)
+    return nothing
 end
 
-function job_leave_sys(job::Int64, node::Int64, time::Float64, state::TrackTotals)
+function job_leave_sys(job::Int64, node::Int64, time::Float64, state::TrackTotals)::Nothing
     # don't need to do anything in this, will already have left node and transit in other calls
 end
 
@@ -61,17 +70,19 @@ function job_join_transit(job::Int64, node::Int64, state::TrackAllJobs)
     state.currentPosition[job] = (arr_time(job, state), -1)
 end   
 
-function job_join_transit(job::Int64, node::Int64, state::TrackTotals)
-    state.transit += 1
+function job_join_transit(job::Int64, node::Int64, state::TrackTotals)::Int64
+    return state.transit += 1
 end
 
 function job_leave_transit end
 
 function job_leave_transit(job::Int64, state::TrackAllJobs)
     # do nothing
+    # unless I implement a collection storing jobs in transit somewhere
 end
 
 function job_leave_transit(job::Int64, state::TrackTotals)
+    @assert state.transit >= 1
     state.transit -= 1
 end
 
@@ -79,20 +90,25 @@ end
 
 function job_join_node(job::Int64, node::Int64, state::TrackAllJobs)
     state.currentPosition[job] = (arr_time(job, state), node)
+    enqueue!(state.buffers[node], job)
 end
 
-function job_join_node(job::Int64, node::Int64, state::TrackTotals)
-    state.atNodes[node] +=1
+function job_join_node(job::Int64, node::Int64, state::TrackTotals)::Int64
+    return state.atNodes[node] +=1
 end
 
 function job_leave_node end
 
-function job_leave_node(job::Int64, node::Int64, state::TrackAllJobs)
+function job_leave_node(job::Int64, node::Int64, state::TrackAllJobs)::Int64
     # do nothing, need the new location so will be handled in the join function that is also called
+    return dequeue!(state.buffers[node])
 end
 
-function job_leave_node(job::Int64, node::Int64, state::TrackTotals)
+function job_leave_node(job::Int64, node::Int64, state::TrackTotals)::Int64
+    #println("node: $node number: $(state.atNodes[node]) : atnodes: $(state.atNodes)")
+    @assert state.atNodes[node] >= 1
     state.atNodes[node] -= 1
+    return 1
 end
 
 
@@ -102,6 +118,40 @@ function arr_time(job::Int64, state::TrackAllJobs)::Float64
     if haskey(state.currentPosition, job)
         return state.currentPosition[job][1]
     else
-        throw(error("Job is not currently in system"))
+        throw(error("Job $job is not currently in system"))
     end
+end
+
+
+
+
+function get_served(node::Int64, state::TrackAllJobs)::Int64
+    return first(state.buffers[node])
+end
+
+function get_served(node::Int64, state::TrackTotals)::Int64
+    @assert length(state.atNodes[node]) > 0
+    return 1
+end
+
+
+
+
+
+function check_capacity(node::Int64, params::NetworkParameters, state::TrackAllJobs)::Bool
+    return params.K[node] == -1 || length(state.buffers[node]) < params.K[node] + 1
+end
+
+function check_capacity(node::Int64, params::NetworkParameters, state::TrackTotals)::Bool
+    return params.K[node] == -1 || state.atNodes[node] < params.K[node] + 1
+end
+
+
+# includes being served
+function jobs_at_node(node::Int64, state::TrackAllJobs)::Int64
+    return length(state.buffers[node])
+end
+
+function jobs_at_node(node::Int64, state::TrackTotals)::Int64
+    return state.atNodes[node]
 end
